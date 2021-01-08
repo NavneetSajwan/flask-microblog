43f428877647e0825883b909eb6fc92fbd94e63a
Navneet Sajwan <iamnavneet619@gmail.com>
2021-01-09 02:18:26 +0530

    password reset feature added

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
+mail = Mail(app)
 
 if not app.debug:
     #config to receive errors to email
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime
 from app import db
 from flask_login import UserMixin
@@ -6,6 +7,10 @@ from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from hashlib import md5
+
+from time import time
+import jwt
+from app import app
 
 @login.user_loader
 def load_user(id):
@@ -51,8 +56,22 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
-        
-    
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(tosken, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
